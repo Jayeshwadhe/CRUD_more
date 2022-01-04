49d85c7e258eb179b2fb176be392a9ba36ad9eff
user <jayeshwadhe>
2022-01-04 19:05:54 +0530

    code update 6

--- app.js
+++ app.js
@@ -3,13 +3,30 @@ const app = express()
 const authroute = require('./routes/user_routes')
 const employeeroute = require('./routes/employee_routes')
 const bodyparser = require('body-parser')
-const swaggerDocument=require('./swagger.json');
+//const swaggerDocument=require('./swagger.json');
+const swaggerJSDoc=require('swagger-jsdoc');
 const swaggerUi=require('swagger-ui-express');
 
+const swaggerOptions={
+    definition:{
+        openapi:'3.0.0',
+        info:{
+            title:'Employee Management API',
+            version:'1.0.0',
+            description:'Employe Api for employee management',
+
+            servers:["http://localhost:3000"]
+        }
+    },
+    apis:["app.js"]
+}
+const swaggerDocs=swaggerJSDoc(swaggerOptions);
+//app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.use(employeeroute)
 app.use(authroute)
--- routes/employee_routes.js
+++ routes/employee_routes.js
@@ -3,6 +3,76 @@ const router = express.Router()
 const controller = require('../controller/employee_controller')
 const auth = require('../middleware/auth_middleware')
 
+
+/**
+ * @swagger
+ * definitions:
+ *  Register:
+ *   type: string
+ *   properties:
+ *    name:
+ *     type: string
+ *     description: name of the employee
+ *     example: 'Jayaramachandran'
+ *    email:
+ *     type: string
+ *     description: email of the employee
+ *     example: 'jayaramachandran@whizpath.com'
+ *    password:
+ *     type: string
+ *     description: password of the employee
+ *     example: 'jayesh@123'
+ *    phoneno:
+ *     type: string
+ *     description: phoneno. of the employee
+ *     example: '8770132674'
+ *    salary:
+ *     type: string
+ *     description: salary of the employee
+ *     example: '2500'
+ *    gender:
+ *     type: string
+ *     description: gender of the employee
+ *     example: 'male'
+ *     birthday:
+ *     type: string
+ *     description: birthday of the employee
+ *     example: '08-11-1998'
+ *     city:
+ *     type: string
+ *     description: city of the employee
+ *     example: 'indore'
+ *  login:
+ *   type: string
+ *   properties:
+ *    email:
+ *     type: string
+ *     description: email of the employee
+ *     example: 'jayaramachandran@whizpath.com'
+ *    password:
+ *     type: string
+ *     description: password of the login employee
+ *     example: 'employee@123'
+ */
+
+
+ /**
+  * @swagger
+  * /Register:
+  *  post:
+  *   summary: create employee
+  *   description: create employee for the organisation
+  *   requestBody:
+  *    content:
+  *     application/json:
+  *      schema:
+  *       $ref: '#/definitions/e_users'
+  *   responses:
+  *    200:
+  *     description: employee created succesfully
+  *    500:
+  *     description: failure in creating employee
+  */
 router.post('/Register',controller.Register)
 router.post('/login',controller.login)
 router.put('/employee_update',auth,controller.employee_update)
